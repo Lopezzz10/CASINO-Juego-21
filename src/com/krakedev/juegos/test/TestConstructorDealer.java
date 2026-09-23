eb5430ef87faac395401898e9559074b3d3685f6
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-09-23 17:38:04 -0500

    agregar prueba inicial de generación del naipe en Dealer

---
+++ src/com/krakedev/juegos/test/TestConstructorDealer.java
@@ -0,0 +1,12 @@
+package com.krakedev.juegos.test;
+
+import com.krakedev.juegos.servicios.Dealer;
+
+public class TestConstructorDealer {
+
+	public static void main(String[] args) {
+		Dealer d1 = new Dealer();
+		d1.imprimirNaipe();
+	}
+
+}
