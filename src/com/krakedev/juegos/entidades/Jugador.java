4b840bf0e4e115e3eed2e13ba30fd48203eff451
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-09-23 17:47:06 -0500

    Crear clase Jugador con recibirCarta()

---
+++ src/com/krakedev/juegos/entidades/Jugador.java
@@ -0,0 +1,29 @@
+package com.krakedev.juegos.entidades;
+
+import java.util.ArrayList;
+
+public class Jugador {
+
+	private String nickname;
+	private ArrayList<Carta> cartas = new ArrayList<>();
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public ArrayList<Carta> getCartas() {
+		return cartas;
+	}
+
+	public void setCartas(ArrayList<Carta> cartas) {
+		this.cartas = cartas;
+	}
+
+	public void recibirCarta(Carta carta) {
+		this.cartas.add(carta);
+	}
+}
